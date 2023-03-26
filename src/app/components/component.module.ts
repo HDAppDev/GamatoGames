a9a4f31b53b2be18671ff3b2e172f461902fc1d0
dongarehari <donhari541@gmail.com>
2023-03-26 23:21:28 +0530

    UI Building and package setup

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -2,11 +2,14 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { IonicModule } from "@ionic/angular";
 import { TopHeaderComponent } from "./top-header/top-header.component";
+import { WelcomePage } from "./welcome/welcome.page";
 
 
 @NgModule({
     declarations: [
         TopHeaderComponent,
+        WelcomePage,
+        TopHeaderComponent
     ],
     exports: [
         TopHeaderComponent
