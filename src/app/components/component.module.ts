b0da2b56b13ced811bc88b5f8299a97cd51f8357
dongarehari <donhari541@gmail.com>
2023-03-31 00:28:43 +0530

    header component

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -2,6 +2,9 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { IonicModule } from "@ionic/angular";
 import { MaterialModule } from "../home/material/material.module";
+import { HeaderComponent } from "./header/header.component";
+import { LoadingScreenComponent } from "./loading-screen/loading-screen.component";
+import { LoadingComponent } from "./loading/loading.component";
 // import { LoginPage } from "./login/login.page";
 import { SignupPage } from "./signup/signup.page";
 import { TopHeaderComponent } from "./top-header/top-header.component";
@@ -14,6 +17,9 @@ import { WelcomePage } from "./welcome/welcome.page";
         WelcomePage,
         TopHeaderComponent,
         SignupPage,
+        LoadingComponent,
+        LoadingScreenComponent,
+        HeaderComponent
     ],
     exports: [
         TopHeaderComponent,
