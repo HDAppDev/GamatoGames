7b46bb14b0c4818063ba369c3312fcfae2ecfd6f
dongarehari <60749222+dongarehari@users.noreply.github.com>
2023-03-31 21:45:51 +0530

    account page setup (#11)

---
+++ src/app/pages/account/account.page.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-account",
+    templateUrl: "./account.page.html",
+    styleUrls: ["./account.page.scss"]
+})
+
+export class AccountPage {
+    
+}
