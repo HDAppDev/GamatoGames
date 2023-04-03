6c04c92b5d0203c23a65acb380801960149b3d86
dongarehari <donhari541@gmail.com>
2023-04-03 22:40:31 +0530

    close account compoent

---
+++ src/app/components/close-account/close-account.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-close-account",
+    templateUrl: "./close-account.component.html",
+    styleUrls: ["./close-account.component.scss"]
+})
+export class CloseAccountComponent {}
