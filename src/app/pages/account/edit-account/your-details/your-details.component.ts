361eb3f0228c1e9d35a78b3dc6a883c1b8dbcdfe
dongarehari <donhari541@gmail.com>
2023-04-03 22:19:32 +0530

    account page UIs

---
+++ src/app/pages/account/edit-account/your-details/your-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-your-details",
+    templateUrl: "your-details.component.html",
+    styleUrls: ["./your-details.component.scss"]
+})
+export class YourDetailsComponent{}
