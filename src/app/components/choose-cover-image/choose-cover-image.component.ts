962660de3032e14e52a4b9c079dfc8c71e3b1295
dongarehari <donhari541@gmail.com>
2023-04-05 02:04:29 +0530

    Cover image component part 1

---
+++ src/app/components/choose-cover-image/choose-cover-image.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-choose-cover-image",
+    templateUrl: "./choose-cover-image.component.html",
+    styleUrls: ["./choose-cover-image.component.scss"]
+})
+export class ChooseCoverImage {}
