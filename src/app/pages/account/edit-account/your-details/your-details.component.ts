9d6b7376d0632981d0df3b00756fd670758a8d4e
dongarehari <donhari541@gmail.com>
2023-05-03 22:49:55 +0530

    new pacckage date-fns and it's implementation

--- src/app/pages/account/edit-account/your-details/your-details.component.ts
+++ src/app/pages/account/edit-account/your-details/your-details.component.ts
@@ -18,7 +18,21 @@ export class YourDetailsComponent{
 
     private initializeForm() {
         this.accountDetails = this.fb.group({
-            profile_nickname: ["", [Validators.required, this.pro]]
+            profile_nickname: ["", [Validators.required, this.profanityValidation.bind(this)]],
+            name: ["", [Validators.required, Validators.minLength(4), Validators.pattern("^[a-zA-Z]+")]],
+            surname: ["", [Validators.required, Validators.minLength(4), Validators.pattern("^[a-zA-Z]+")]],
+            address_1: ["", [Validators.required, Validators.minLength(4)]],
+            address_2: ["", [Validators.required, Validators.minLength(4)]],
+            currentLanguage: [localStorage.getItem("language_code")],
+            postal_code: ["", [Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+            town: ["", [Validators.required]],
+            // date_of_birth: ["", Validators.compose([this.dateOfBirthValidation.bind(this)])],
+            email: [
+                "",
+                Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+            ],
+            msn: [""],
+
         })
     }
 
@@ -30,5 +44,20 @@ export class YourDetailsComponent{
             }
         }
         return null
+    }
+
+    dateOfBirthValidation(control: UntypedFormControl) {
+        if(control.value && control.value != "Invalid date" && control.value != "") {
+            let now = new Date();
+            let dob = new Date(control.value);
+            if(now < dob) {
+                return {futureInvalidDate: true};
+            }
+
+        }
+        else if (control.value == "Invalid date") {
+            return {futureInvalidDate: true}
+        }
+        return null;
     }
 }
