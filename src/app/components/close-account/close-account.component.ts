2f856420f82c2bee6f08b122e7cf277d2311e84f
dongarehari <donhari541@gmail.com>
2023-05-05 23:37:31 +0530

    update close account component

--- src/app/components/close-account/close-account.component.ts
+++ src/app/components/close-account/close-account.component.ts
@@ -1,8 +1,34 @@
 import { Component } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
+import { AuthService } from "src/app/core/auth.service";
+import { TranslateService } from "@ngx-translate/core"
 
 @Component({
     selector: "app-close-account",
     templateUrl: "./close-account.component.html",
     styleUrls: ["./close-account.component.scss"]
 })
-export class CloseAccountComponent {}
+export class CloseAccountComponent {
+    constructor(private dialog: MatDialogRef<CloseAccountComponent>,
+        private authService: AuthService,
+        public translate: TranslateService) {
+        try {
+            this.authService?.selectedlanguages$.subscribe((l) => {
+                translate.use(l ? l: "en");
+            }, (err => {
+                return err;
+            }));
+        } catch(err) {
+            // return err;
+        }
+
+    }
+
+    cancel() {
+        this.dialog.close(false);
+    }
+
+    closeAccount() {
+        this.dialog.close(true);
+    }
+}
--- src/app/components/holding-screen/holding-screen.component.ts
+++ src/app/components/holding-screen/holding-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   selector: 'app-holding-screen',
@@ -6,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./holding-screen.component.scss'],
 })
 export class HoldingScreenComponent implements OnInit {
+  seedingPending = [];
+  seedingComplete = [];
+  completed$ = new BehaviorSubject(false);
+  selectedLanguage: string = "en";
 
-  constructor() { }
+  redirectTo: any;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {}
 
