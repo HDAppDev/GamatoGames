752bdd941269c8c4d8bd9a172e86e09a242eca6d
dongarehari <donhari541@gmail.com>
2023-07-19 13:55:10 +0530

    Adding font to website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { WelcomePage } from './components/welcome/welcome.page';
 
 const routes: Routes = [
+  
+  {
+    path: "",
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
   {
     path: 'home',
     loadChildren: () =>
       import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+    path: "welcome",
+    component: WelcomePage,
+    data: {
+    }
+  },  
   {
     path: 'games',
     loadChildren: () =>
